ac4ace2faebd1359d896e563606a330a090b3c24
DYoshizawa079 <daniely079@gmail.com>
2022-03-22 13:40:23 -0400

    Another fix to server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { authMiddleware } = require("./utils/auth");
 const path = require("path");
 const db = require("./config/connection");
 
-const { typeDefs, resolvers } = require("./schema/");
+const { typeDefs, resolvers } = require("./schemas/");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
