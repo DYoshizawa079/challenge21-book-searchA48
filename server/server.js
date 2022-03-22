d4c46a777766e84f4214e8f9a32bb717658ac73a
DYoshizawa079 <daniely079@gmail.com>
2022-03-22 13:34:21 -0400

    Fixed server.js for heroku

--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
+
 const path = require("path");
-const db = require('./config/connection');
+const db = require("./config/connection");
 
-// Import authentication middleware
-const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require("./schema/");
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({req}) => req.headers
+    context: authMiddleware
   });
   await server.start();
   server.applyMiddleware({ app });
@@ -27,8 +27,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  // send index.html for any routes that don't exist
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 }
 
 db.once('open', () => {
